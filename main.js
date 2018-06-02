fd5ecf6d835c889b64bc27cbe14d66002088d0e8
Bharat Middha <bmiddha99@gmail.com>
2018-06-02 00:40:17 -0500

    Added time date, events, org data, motd, highlights, weather

---
+++ main.js
@@ -0,0 +1,159 @@
+const MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+const DAYS = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+const orgs = ["acm", "lug", "wics", "uiccs"];
+const orgData = [];
+orgData["acm"] = {
+	name: "Association for Computing Machinery",
+	logo: "images/acmLogo.png",
+	acronym: "ACM",
+	motd: "files/acm.motd",
+	highlights: "files/acm.highlights",
+	calendar: ""
+};
+orgData["lug"] = {
+	name: "Linux Users Group",
+	logo: "images/lugLogo.png",
+	acronym: "LUG",
+	motd: "files/lug.motd",
+	highlights: "files/lug.highlights",
+	calendar: ""
+};
+orgData["wics"] = {
+	name: "Women in Computer Science",
+	logo: "images/wicsLogo.png",
+	acronym: "WiCS",
+	motd: "files/wics.motd",
+	highlights: "files/wics.highlights",
+	calendar: ""
+};
+orgData["uiccs"] = {
+	name: "UIC Computer Science",
+	logo: "images/uicCsLogo.png",
+	acronym: "CS",
+	motd: "files/uicCs.motd",
+	highlights: "files/uicCs.highlights",
+	calendar: ""
+};
+
+function getApiData(type, arg, value) {
+	return new Promise((resolve, reject) => {
+		let xhr = new XMLHttpRequest();
+		let url = "http://localhost:8080/api/" + type + "/?" + arg + "=" + value;
+		xhr.open("GET", url, true);
+		xhr.send();
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState == 4 && xhr.status == 200) {
+				let data;
+				if (xhr.responseText) {
+					try {
+						data = JSON.parse(xhr.responseText);
+					} catch (e) {
+						console.log(e);
+					}
+				}
+				resolve(data);
+			}
+		};
+	});
+}
+
+function addZero(i) {
+	return (i < 10) ? ("0" + i) : i;
+}
+
+function ampm(i) {
+	return (i > 12) ? "AM" : "PM";
+}
+
+function timeAndDate() {
+	let today = new Date();
+	let hour = today.getHours();
+	let min = today.getMinutes();
+	let date = today.getDate();
+	let month = today.getMonth() + 1;
+	let day = DAYS[today.getDay()];
+	hour = (hour > 12) ? (hour - 12) : hour;
+	min = addZero(min);
+	hour = addZero(hour);
+	document.querySelector("#time").innerHTML = hour + ":" + min + " " + day + ", " + MONTHS[month] + " " + date;
+}
+
+function getEvents(cal) {
+	getApiData("calendar", "cal", cal).then((result) => {
+		let k = 1;
+		document.querySelector("#events").innerHTML = "";
+		while (k < result.length) {
+			if (k > 4) break;
+			let dtStart = new Date(result[k].timeStart);
+			let tmStartHour = dtStart.getHours();
+			let tmStartAmPm = "AM";
+			if (tmStartHour > 12) {
+				tmStartHour -= 12;
+				tmStartAmPm = "PM";
+			}
+			tmStartHour = addZero(tmStartHour);
+			tmStart = DAYS[dtStart.getDay()] + ", " + MONTHS[dtStart.getMonth()] + " " + dtStart.getDate() + " " + tmStartHour + ":" + addZero(dtStart.getMinutes()) + tmStartAmPm;
+			let dtEnd = new Date(result[k].timeEnd);
+			let tmEndHour = dtEnd.getHours();
+			let tmEndAmPm = "AM";
+			if (tmEndHour > 12) {
+				tmEndHour -= 12;
+				tmEndAmPm = "PM";
+			}
+			tmEndHour = addZero(tmEndHour);
+			tmEnd = DAYS[dtEnd.getDay()] + ", " + MONTHS[dtEnd.getMonth()] + " " + dtEnd.getDate() + " " + tmEndHour + ":" + addZero(dtEnd.getMinutes()) + tmEndAmPm;
+			document.querySelector("#events").innerHTML += "<li><span class=summary>" + result[k].summary + "</span>"
+			document.querySelector("#events").innerHTML += "<span class=location>" + result[k].location + "</span>"
+			document.querySelector("#events").innerHTML += "<span class=timeStart>" + tmStart + "</span>"
+			document.querySelector("#events").innerHTML += "<span class=timeEnd>" + tmEnd + "</span>"
+			document.querySelector("#events").innerHTML += "<span class=description>" + result[k].description + "</span></li>";
+			k++;
+		}
+	});
+}
+
+function getMotd(file) {
+	getApiData("file", "file", file).then((result) => {
+		document.querySelector("#motd").innerHTML = result.data;
+	});
+}
+
+function getHighlights(file) {
+	getApiData("file", "file", file).then((result) => {
+		document.querySelector("#highlights").innerHTML = result.data;
+	});
+}
+
+function getWeather(city) {
+	getApiData("weather", "city", city).then((result) => {
+		let temp = result.main.temp;
+		let tempF = Math.round(result.main.temp * 9 / 5 - 459.67);
+		let tempC = Math.round(result.main.temp - 273.15);
+		document.querySelector("#weather").innerHTML = result.weather[0].main + "<br>" + tempF + "&#176;F | " + tempC + "&#176;C";
+	});
+}
+
+getWeather("chicago");
+timeAndDate();
+
+
+var activeOrg = "";
+var counter = 0;
+
+function updateActiveOrg() {
+	activeOrg = counter % orgs.length;
+	// console.log(orgData[orgs[activeOrg]]);
+	counter++;
+	getMotd(orgData[orgs[activeOrg]].motd);
+	getHighlights(orgData[orgs[activeOrg]].highlights);
+	getEvents(orgData[orgs[activeOrg]].calendar);
+	document.querySelector("#org-name").innerHTML = orgData[orgs[activeOrg]].name;
+	document.querySelector("#org-logo>img").src = orgData[orgs[activeOrg]].logo;
+	console.log(activeOrg);
+	// let a = "footer>div:nth-child("+ activeOrg )";
+	// console.log(document.querySelector(a).classList);
+	setTimeout(updateActiveOrg, 3000)
+}
+
+
+updateActiveOrg();
---
+++ server.js
@@ -0,0 +1,94 @@
+const dotenv = require('dotenv');
+const ical = require('ical');
+const http = require("http");
+const fs = require("fs");
+const express = require("express");
+const app = express();
+const port = 8080;
+const openWeatherApiKey = process.env.OPEN_WEATHER_MAP_API_KEY;
+app.listen(port);
+console.log("Server started on port " + port);
+
+function getEvents(calendarUrl) {
+	return new Promise((resolve, reject) => {
+		ical.fromURL(calendarUrl, {}, (err, data) => {
+			let eventData = [];
+			for (let k in data) {
+				if (data.hasOwnProperty(k)) {
+					let ev = data[k];
+					let event = {
+						summary: ev.summary,
+						timeStart: ev.start,
+						timeEnd: ev.end,
+						location: ev.location,
+						description: ev.description
+					};
+					eventData.push(event);
+				}
+			}
+			resolve(eventData);
+		});
+	});
+}
+
+function getFromUrl(url) {
+	return new Promise((resolve, reject) => {
+		http.request(url, (response) => {
+			var str = "";
+			response.on("data", (chunk) => {
+				str += chunk;
+			});
+			response.on("end", () => {
+				resolve(str);
+			});
+		}).end();
+	});
+}
+
+function getWeather(city) {
+	return new Promise((resolve, reject) => {
+		var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + openWeatherApiKey;
+		getFromUrl(weatherUrl).then((result) => {
+			let weatherJson = JSON.parse(result);
+			resolve(weatherJson);
+		});
+	});
+}
+
+function getFile(file) {
+	return new Promise((resolve, reject) => {
+		let fileData = {};
+		fs.readFile(file, 'utf8', (err, data) => {
+			data = data.replace(/(?:\r\n|\r|\n)/g, '<br>');
+			fileData = {
+				data: data
+			};
+			resolve(fileData);
+		});
+	});
+}
+
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+app.get("/api/calendar", (req, res) => {
+	let cal = req.query.cal;
+	getEvents(cal).then((result) => {
+		res.send(result);
+	});
+});
+app.get("/api/file", (req, res) => {
+	let file = req.query.file;
+	getFile(file).then((result) => {
+		res.send(result);
+	});
+});
+app.get("/api/weather", (req, res) => {
+	let city = req.query.city;
+	getWeather(city).then((result) => {
+		res.send(result);
+	});
+});
