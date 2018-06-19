1c48bee3d64026f2f763ab7c9090ebe992cc850c
Bharat Middha <bmiddha99@gmail.com>
2018-06-19 15:06:01 -0500

    serving static files with express.js

--- server.js
+++ server.js
@@ -73,6 +73,7 @@ function getFile(file) {
 
 const cors = require('cors');
 app.use(cors());
+app.use(express.static('public'));
 app.use(function (req, res, next) {
 	// res.header("Access-Control-Allow-Origin", "*");
 	// res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
